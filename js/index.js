57dde746fa1f658d4a723f97c83fced3d4b1c5f3
Hector-bit <hectormartinez.2978@gmail.com>
2019-09-03 21:52:50 -0700

    added 5 more event listeners

--- js/index.js
+++ js/index.js
@@ -9,9 +9,39 @@ firstjs.addEventListener('mouseover', (e) => {
 //second addeventlistener
 const secondjs = document.querySelector('body')
 
-secondjs.addEventListener('keydown', (e) =>{
+secondjs.addEventListener('keydown', (e) => {
     secondjs.style.backgroundColor = 'grey';
-})
+});
 
 //third addEventListener
-const thirdhs = document.querySelector('')
+const thirdjs = document.querySelector('.intro p')
+
+thirdjs.addEventListener('click', (e) => {
+    thirdjs.style.fontSize = '24px';
+});
+
+//fourth addEventListener
+const fourthjs = document.querySelector('p')
+
+fourthjs.addEventListener(`dblclick` , (e) => {
+    fourthjs.style.border = '3px solid purple';
+    console.log('asdf')
+});
+
+//fifth addEventListener
+const fifthjs = document.querySelector('.img-content')
+
+fifthjs.addEventListener('mouseleave', (e) => {
+    fifthjs.style.display = 'none';
+    console.log('ghfdj');
+})
+
+//sixth addEventListener
+const sixthjs = document.querySelector('.img-fluid')
+
+sixthjs.addEventListener('mouseover', (e) => {
+    sixthjs.src = 'https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940';
+    console.log('erd');
+})
+
+//seventh addEventListener
