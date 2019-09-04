3243a0c3eaec6140dd68db752cf371478a078141
Hector-bit <hectormartinez.2978@gmail.com>
2019-09-03 19:55:03 -0700

    added two event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+const firstjs = document.querySelector('.intro img');
+
+firstjs.addEventListener('mouseover', (e) => {
+    firstjs.style.border = "4px solid red";
+    console.log('hgjjknkjsdf');
+});
+
+//second addeventlistener
+const secondjs = document.querySelector('body')
+
+secondjs.addEventListener('keydown', (e) =>{
+    secondjs.style.backgroundColor = 'grey';
+})
+
+//third addEventListener
+const thirdhs = document.querySelector('')
